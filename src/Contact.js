e0ec224a77570ce3db6840f8b7306a755294b19a
Saras <78587510+sarasverma@users.noreply.github.com>
2022-04-03 09:18:40 +0530

    Skeleton ready

---
+++ src/Contact.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Contact() {
+  return <div className="container">This is contact.</div>;
+}
+
+export default Contact;
---
+++ src/components/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About() {
+  return <div className="container">This is about</div>;
+}
+
+export default About;
